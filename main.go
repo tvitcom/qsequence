ee54783ec7d10c3a7d26c99473c57487bbb87500
r <you@example.com>
2021-04-05 17:52:11 +0300

    replace to the function

--- main.go
+++ main.go
@@ -3,12 +3,33 @@ package main
 import (
     "fmt"
     "math/big"
+    // "strconv"
     "crypto/rand"
 )
 
-func main() {
-    xs := []uint64{1,2,3,4,5,6,7,8,9,10,11,12,12,14,15,16,17,18,19,20}
+const (
+    QUERY_CHAIN_LENGTH = 20 // 20 вопросов может быть максимально задано
+)
+type (
+    QueryElement struct {
+        QueryChain string
+        Tries uint64
+    }
+)
 
+func main() {
+
+    queueNumbers := []uint64{1,2,3,4,5,6,7,8,9,10,11,12,12,14,15,16,17,18,19,20,}
+    
+
+    qQueueQuestionNumber, stringQueue := getQueueQuestionNumbers(queueNumbers, false)
+    fmt.Println(stringQueue)
+    fmt.Println(qQueueQuestionNumber)
+}
+
+func getQueueQuestionNumbers(xs []uint64, debug bool) ([]uint64, string) {
+    queueString := ""
+    var qQueueNumbers []uint64
     shuffleElement := func() uint64 {
         // get rand number for index
         b := len(xs)
@@ -19,14 +40,23 @@ func main() {
         }
         index := uint64(num.Int64())
         result := xs[index] 
+        queueString = queueString + fmt.Sprintf("%v,", xs[index])
         xs = append(xs[:index], xs[index+1:]...)
         return result 
     }
 
     lenXs := len(xs)
+    next := uint64(0)
     fmt.Println("lenXs:",lenXs)
-    for i :=0 ;i < lenXs;i++ {
-        fmt.Println(shuffleElement())
-    }
-}
 
+    for i :=0; i < lenXs; i++ {
+        next = shuffleElement()
+        if (debug) {
+            fmt.Println(next, queueString)
+        }
+        qQueueNumbers = append(qQueueNumbers, next)
+    }
+
+    return qQueueNumbers, queueString
+
+}
