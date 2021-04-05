ff1a25fcffa8aa6460d0974a1370eb4a98d85185
r <you@example.com>
2021-04-05 18:18:39 +0300

    implement checking sequence occured in pudge DB

--- main.go
+++ main.go
@@ -2,29 +2,55 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"math/big"
-	// "strconv"
 	"crypto/rand"
+	"github.com/recoilme/pudge"
 )
 
 const (
 	QUERY_CHAIN_LENGTH = 20 // 20 вопросов может быть максимально задано
 )
 
-type (
-	QueryElement struct {
-		QueryChain string
-		Tries      uint64
-	}
-)
+// type (
+// 	QueryElement struct {
+// 		QueryChain string
+// 		Tries      uint64
+// 	}
+// )
 
 func main() {
-
 	queueNumbers := []uint64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 12, 14, 15, 16, 17, 18, 19, 20}
+	qQueueQuestionNumbers, stringQueue := getQueueQuestionNumbers(queueNumbers, false)
 
-	qQueueQuestionNumber, stringQueue := getQueueQuestionNumbers(queueNumbers, false)
-	fmt.Println(stringQueue)
-	fmt.Println(qQueueQuestionNumber)
+	dbFile := "../data/pudgedb"
+	cfg := &pudge.Config{
+		SyncInterval: 1,
+		// StoreMode: 2,
+	}//disable every second fsync
+	db, err := pudge.Open(dbFile, cfg)
+	if err != nil {
+	    log.Panic(err)
+	}
+	defer db.DeleteFile()
+
+	// Wait for interrupt signal to gracefully shutdown the server 
+	// quit := make(chan os.Signal)
+	// signal.Notify(quit, os.Interrupt, os.Kill)
+	// <-quit
+	// log.Println("Shutdown Server ...")
+	// if err := pudge.CloseAll(); err != nil {
+	// 	log.Println("Pudge Shutdown err:", err)
+	// }
+
+	if ok, _ := db.Has(stringQueue); !ok {
+		pudge.Set(dbFile, stringQueue, qQueueQuestionNumbers)
+	} else {
+		log.Println("Sequence last occured before. Try again.")
+	}
+
+	log.Println(stringQueue)
+	log.Println(qQueueQuestionNumbers)
 }
 
 func getQueueQuestionNumbers(xs []uint64, debug bool) ([]uint64, string) {
@@ -49,13 +75,13 @@ func getQueueQuestionNumbers(xs []uint64, debug bool) ([]uint64, string) {
 	next := uint64(0)
 
 	if debug {
-		fmt.Println("lenXs:", lenXs)
+		log.Println("lenXs:", lenXs)
 	}
 
 	for i := 0; i < lenXs; i++ {
 		next = shuffleElement()
 		if debug {
-			fmt.Println(next, queueString)
+			log.Println(next, queueString)
 		}
 		qQueueNumbers = append(qQueueNumbers, next)
 	}
